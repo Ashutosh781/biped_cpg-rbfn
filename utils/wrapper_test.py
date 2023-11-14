d7f39b381e30df5928c329af98a6678ed22f73c9
Magraz <agrazvallejo@live.com>
2023-11-14 13:28:22 -0800

    updated wrapper_test

--- utils/wrapper_test.py
+++ utils/wrapper_test.py
@@ -2,39 +2,42 @@
 
 import os
 import sys
+import json
+import random as rand
 
 # Add project root to the python path
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
-from utils.ann_lib import AdaptiveSO2CPGSynPlas, Neuron
-
-# ann = ANN(5)
-
-# ann.setInput(4, 1)
-# ann.setWeight(1, 2, 5)
-
-# print(ann.dumpBiases())
-
-# print(ann.dumpWeights())
+from utils.ann_lib import AdaptiveSO2CPGSynPlas, Neuron, postProcessing, cpg_rbfn
 
 M_PI=3.1415
 
-neur = Neuron()
+data = {}
+with open("./data/RL_job_base.json") as file:
+    data = json.load(file)
 
-cpg = AdaptiveSO2CPGSynPlas(neur)
+weights = [data["ParameterSet"]]
+sensorWeights = [data["SensorParameterSet"]]
+encoding = data["checked"]
 
-cpg.setPhi     ( 0.02*M_PI )   # Frequency term - Influences w00 w01 w10 w11 of the SO(2) oscillator (long term)
-cpg.setEpsilon ( 0.1 )        # Value should depend on the initial and external freq - from P to h2 (short term)
-cpg.setAlpha   ( 1.01)        # Amplitude and linearity between phi and the frequency
-cpg.setGamma   ( 1.0 )        # Synaptic weight from h2 to h0 - Governed by a Hebbian-type learning (short term)
-cpg.setBeta    ( 0.0 )        # Synaptic weight from h0 to h2 - Governed by a Hebbian-type learning (short term)
-cpg.setMu      ( 1.0 )        # Learning rate - Value should depend on the given initial and external freq
-cpg.setBetaDynamics( -1.0, 0.010, 0.00) # Heppian Rate, Decay Rate, Beta_0
-cpg.setGammaDynamics( -1.0, 0.010, 1.00) # --- || ---
-cpg.setEpsilonDynamics(  1.0, 0.010, 0.01) # --- || ---
+CPG_RBFN = cpg_rbfn(weights, encoding, 20, "walk", sensorWeights)
+startPhi = 0.015 * M_PI
+CPG_RBFN.setPhii(startPhi)
 
-#destabilize cpg to oscillate
-cpg.setOutput(0,0.2012)
-cpg.setOutput(1,0)
+CPGPeriodPostprocessor = postProcessing()
 
-print(cpg.getGamma())
+# Calculate init period
+# We use random number to let the CPG start at different time = improved robustness
+tau = 1
+randomNumber = int(rand.uniform(1, tau) + 1)  #Random number between 1 and tau // was = 1
+for i in range(0,tau+randomNumber):
+    CPGPeriodPostprocessor.calculateAmplitude(CPG_RBFN.getCpgOutput(0), CPG_RBFN.getCpgOutput(1))
+    CPGPeriod = int(CPGPeriodPostprocessor.getPeriod())
+    CPG_RBFN.setCPGPeriod(CPGPeriod)
+
+    sensorOutput = []
+    CPG_RBFN.step(sensorOutput)
+
+    RBF_output = CPG_RBFN.getNetworkOutput()
+
+    print(RBF_output)
